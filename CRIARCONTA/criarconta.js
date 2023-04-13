07f6666e7350e78af1a29f27bbc744d4a9e871c7
KalfelsWiggers <gabriel_wiggers@estudante.sesisenai.org.br>
2023-04-13 11:05:15 -0300

    a

---
+++ CRIARCONTA/criarconta.js
@@ -0,0 +1,16 @@
+function ValidarCPF() 
+{
+    expressao = /^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$/g;
+    texto = cpf.value
+    resposta = expressao.test(texto);
+    if (resposta == false) 
+    {
+        cpf.classList.remove('certo');
+        cpf.classList.add('errado');
+    }
+    if (resposta == true) 
+    {
+        cpf.classList.remove('errado');
+        cpf.classList.add('certo');
+    }
+}
---
+++ HUB/HUB.js
@@ -0,0 +1,30 @@
+function ValidarCEP() 
+{
+    expressao = /^[0-9]{5}-[0-9]{3}$/g;
+    texto = cep.value
+    resposta = expressao.test(texto);
+    if (resposta == false) 
+    {
+        cep.classList.remove('certo');
+        cep.classList.add('errado');
+    }
+    if (resposta == true) 
+    {
+        cep.classList.remove('errado');
+        cep.classList.add('certo');
+    }
+}
+function calcularFrete() 
+{
+    expressao = /^[0-9]{5}-[0-9]{3}$/g;
+    texto = cep.value
+    resposta = expressao.test(texto);
+    if (resposta == true) 
+    {
+        const cep = document.getElementById("cep").value;
+        const frete = Math.random() * 50; // valor de frete fictício
+        const freteFormatado = frete.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        document.getElementById("frete").textContent = `O valor do frete para o CEP: ${cep} é de ${freteFormatado}.`;
+    }
+
+}
